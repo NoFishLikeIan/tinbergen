e5bf756c8035eef360bc58b25f0dcbaa064ddb9d
NoFishLikeIan <andreatitton96@gmail.com>
2020-05-07 12:03:44 +0200

    📈 basic simulation

--- code/thesis_ideas/main.py
+++ code/thesis_ideas/main.py
@@ -1,14 +1,66 @@
-from rr_model.model import Industry
-from rr_model.network import Network
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import pandas as pd
+import networkx as nx
 
 from mpl_toolkits import mplot3d
 from matplotlib import animation
 
+from rr_model.model import Industry
+from rr_model.network import Network
+
 
 sns.set(rc={'figure.figsize': (12, 8)})
+
+
+def simulate(net: Network, iters=150, verbose=True):
+    if verbose:
+        print("Bringing to steady...")
+    net.bring_to_steady(iters=iters, verbose=verbose)
+    n = len(net)
+
+    prev_wage = net[n-1].wage
+    next_wage = prev_wage*2
+    net[n-1].wage = next_wage
+
+    if verbose:
+        print(f"Wage: {prev_wage} -> {next_wage}")
+
+    data = [[] for _ in range(n)]
+
+    base = [net[i].aggregate_prod for i in range(n)]
+
+    if verbose:
+        print("Shock...")
+
+    for _ in range(iters):
+        if verbose:
+            print(f"{_+1}/{iters}", end='\r')
+
+        for i in range(n):
+            net[i].step()
+            prod = net[i].aggregate_prod / base[i]
+            data[i].append(prod)
+
+    net[n-1].wage = prev_wage
+
+    if verbose:
+        print("...recovery...")
+
+    for _ in range(iters):
+        if verbose:
+            print(f"{_+1}/{iters}", end='\r')
+
+        for i in range(n):
+            net[i].step()
+            prod = net[i].aggregate_prod / base[i]
+            data[i].append(prod)
+
+    if verbose:
+        print("...done!")
+
+    return pd.DataFrame(data).T
 
 
 if __name__ == '__main__':
@@ -20,34 +72,29 @@ if __name__ == '__main__':
         "beta": 0.95
     }
 
-    first = Industry(
-        fixed_overhead=overhead,
-        alpha=3,
-        theta_one=theta_one,
-        theta_two=0.3,
-        params=params,
-    )
+    theta_two = 0.2
 
-    second = Industry(
-        fixed_overhead=overhead,
-        alpha=3,
-        theta_one=theta_one,
-        theta_two=0.3,
-        params=params,
-    )
+    inds = []
 
-    third = Industry(
-        fixed_overhead=overhead,
-        alpha=3,
-        theta_one=theta_one,
-        theta_two=0.3,
-        params=params,
-    )
+    for _ in range(5):
+        i = Industry(
+            fixed_overhead=overhead,
+            alpha=3,
+            theta_one=theta_one,
+            theta_two=theta_two,
+            params=params,
+        )
 
-    d = np.array([
-        [0, 0.2, 0],
-        [0, 0, 0],
-        [0.2, 0.8, 0]
-    ])
+        inds.append(i)
 
-    G = Network([first, second, third], d)
+    d = np.tril(np.random.randint(1, 10, size=(5, 5)), -1)
+
+    net = Network(inds, d)
+
+    nx.draw_networkx(net.G)
+    plt.savefig("plots/network.png")
+
+    df = simulate(net, iters=50)
+    df.plot()
+
+    plt.savefig("plots/wage_shock.png")
