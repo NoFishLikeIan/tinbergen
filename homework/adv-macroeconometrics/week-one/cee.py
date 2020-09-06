e9933eeaef29b2ae2f4303716c165f4c8eb347be
NoFishLikeIan <andreatitton96@gmail.com>
2020-09-06 15:28:31 +0000

    🐱‍🏍 residual analysis on cee

--- homework/adv-macroeconometrics/week-one/cee.py
+++ homework/adv-macroeconometrics/week-one/cee.py
@@ -1,32 +1,13 @@
-import pandas as pd
-import numpy as np
-
-from statsmodels.tsa.vector_ar import var_model
-
 from utils.read_data import read_data
 from utils.plot import plot_var
+from utils.var import var
 
-
-def var(df, lags = -1, mode = 'aic'):
-
-    model = var_model.VAR(df, dates=df.index, freq="Q")
-
-    if lags > 0:
-        print(f"Using given lag order ({lags})...")
-        results = model.fit(lags, trend="c", verbose=True)
-        
-    else:
-        print("Finding optimum lag order...")
-        results = model.fit(trend="c", maxlags = 15, ic = mode, verbose=True) 
-
-    return results
 
 if __name__ == "__main__":
-    df = read_data("data/cee.xls")
+    df = read_data("data/cee.xls", f = "1963Q3")
 
     standard_res = var(df, lags = 4)
-    plot_var(standard_res, folder="ex-lag")
+    plot_var(standard_res, folder="ex-cee")
 
-    standard_res = var(df)
-    plot_var(standard_res, folder="aic-lag")
-
+    standard_res = var(df, trend="c", maxlags=15, ic = "aic")
+    plot_var(standard_res, var = "FF", folder="aic-cee")
