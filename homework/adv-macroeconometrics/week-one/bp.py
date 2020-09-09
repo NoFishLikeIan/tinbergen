8346def48e728e1039771113f706666da89fb264
NoFishLikeIan <andreatitton96@gmail.com>
2020-09-09 18:54:53 +0200

    🎉 AB-matrix creation

--- homework/adv-macroeconometrics/week-one/bp.py
+++ homework/adv-macroeconometrics/week-one/bp.py
@@ -3,10 +3,22 @@ import numpy as np
 
 from utils.read_data import read_data
 from utils.plot import plot_var
-from utils.var import var
+from utils.var import var, svar
 
 # FIXME: fix parsing warning
 pd.options.mode.chained_assignment = None 
+
+A = np.array([
+    [1., 0., "E"],
+    ["E", 1., "E"],
+    ["E", "E", 1.],
+])
+
+B = np.array([
+    ["E", 0., 0.],
+    [0., "E", 0.],
+    [0., 0., "E"]
+])
 
 def parse_data(df):
     deflator = df["PGDP"]
@@ -23,9 +35,6 @@ def parse_data(df):
 
         scaled_df[col] = log
 
-    scaled_df["D"] = np.zeros(len(scaled_df))
-    scaled_df["D"]["1975Q2"] = 1
-
     scaled_df = scaled_df.rename(
         columns = {
             "GCN": "G",
@@ -36,11 +45,50 @@ def parse_data(df):
 
     return scaled_df
 
+def make_A_matrix(case, def_A=A):
+    A = def_A.copy()
+
+    if "case" == "f":
+        A[0, 2] = 0.
+        A[1, 2] = -1.5
+
+        return A
+
+    elif "case" == "s":
+        A[0, 2] = -.1
+        A[1, 2] = 0.
+
+        return A
+
+    elif "case" == "t":
+        A[0, 2] = 0.
+        A[1, 0] = 0.
+        A[1, 2] = 0.
+
+        return A
+
+    elif "cholesky":
+        A[0, 1] = 0.
+        A[0, 2] = 0.
+        A[1, 2] = 0.
+
+        return A
+    
+    else:
+        raise ValueError("Case not recognized")
 
 
 if __name__ == "__main__":
     df = read_data("data/bp.xls")
     scaled_df = parse_data(df)
 
-    results = var(scaled_df,  lags=4, trend="ctt")
-    plot_var(results, var="G", folder="bp")
+    for case in ["f", "s", "t", "cholesky"]:
+
+        A_restr = make_A_matrix(case)
+
+        print(f"{case}: A = {A_restr}")
+
+        results = svar(
+            scaled_df, A=A_restr, B=B, trend="ctt")
+
+        plot_var(results, var="G", folder=f"A_{case}")
