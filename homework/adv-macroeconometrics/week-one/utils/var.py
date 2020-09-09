36563611bc79b0936341bd4f85cbf980c545b209
NoFishLikeIan <andreatitton96@gmail.com>
2020-09-09 18:18:40 +0200

    🎉 option for Structural VAR

--- homework/adv-macroeconometrics/week-one/utils/var.py
+++ homework/adv-macroeconometrics/week-one/utils/var.py
@@ -1,8 +1,35 @@
-from statsmodels.tsa.vector_ar import var_model
+import numpy as np
+
+from statsmodels.tsa.vector_ar import var_model, svar_model
 
 def var(df, lags = -1, **kwargs):
 
     model = var_model.VAR(df, dates=df.index, freq="Q")
+
+    if lags > 0:
+        print(f"Using given lag order ({lags})...")
+        results = model.fit(lags, verbose=True, **kwargs)
+        
+    else:
+        print("Finding optimum lag order...")
+        results = model.fit(verbose=True, **kwargs) 
+
+    return results
+
+def svar(df, A = None, B = None, lags = -1, **kwargs):
+
+    if B is None:
+        svar_type = "A"
+    elif A is None:
+        svar_type = "B"
+    else:
+        svar_type="AB"
+
+    model = svar_model.SVAR(
+        df,
+        dates=df.index, 
+        A=A, B=B, svar_type=svar_type
+    )
 
     if lags > 0:
         print(f"Using given lag order ({lags})...")
