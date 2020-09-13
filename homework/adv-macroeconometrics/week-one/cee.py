0a21cd79abea664ea181a9cd63106ccfacab731a
NoFishLikeIan <andreatitton96@gmail.com>
2020-09-13 18:05:33 +0200

    🔧 adapt to plotting changes

--- homework/adv-macroeconometrics/week-one/cee.py
+++ homework/adv-macroeconometrics/week-one/cee.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     df = read_data("data/cee.xls", f = "1963Q3")
 
     standard_res = var(df, lags = 4)
-    plot_var(standard_res, impulse = "FF", folder="cee/ex")
+    plot_var(standard_res, impulse = "FF", response=df.columns, folder="cee/ex", autocorr=True)
 
-    standard_res = var(df, trend="c", maxlags=15, ic = "aic")
-    plot_var(standard_res, impulse = "FF", folder="cee/aic")
+    ic = "bic"
+    standard_res = var(df, trend="c",lags=2)
+    plot_var(standard_res, impulse = "FF", folder=f"cee/{ic}")
