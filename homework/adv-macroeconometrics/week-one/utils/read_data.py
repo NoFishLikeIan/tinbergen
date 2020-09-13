9e8e57b036d3326a23b1d5b9b3dd0dc86f6a321d
NoFishLikeIan <andreatitton96@gmail.com>
2020-09-13 16:26:36 +0200

    🎉 different parsing for frequency

--- homework/adv-macroeconometrics/week-one/utils/read_data.py
+++ homework/adv-macroeconometrics/week-one/utils/read_data.py
@@ -1,10 +1,16 @@
 import pandas as pd
 
 def format_dates(series):
+
+    if type(series[0]) != str:
+        series = series.apply(
+            lambda el: f"{int(el)}:{int((el-int(el)) * 4 + 1)}"
+        )
+
     qs = series.apply(
         lambda q: "-Q".join([str(int(n)) for n in str(q).split(":")])
     )
-    
+
     return pd.PeriodIndex(qs.values, freq="Q")
 
 
@@ -20,7 +26,7 @@ def read_data(name, f = ""):
     else:
         raise ValueError("File type not understood")
 
-    df = df.dropna(axis = 1).rename(columns={"Unnamed: 0": "t"})
+    df = df.dropna(how="all", axis = 1).rename(columns={"Unnamed: 0": "t"})
 
     df["t"] = format_dates(df["t"])
 
