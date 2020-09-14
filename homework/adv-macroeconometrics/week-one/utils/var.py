a06e93c2e8296bebfc55365cd4673f135bbcb9cd
NoFishLikeIan <andreatitton96@gmail.com>
2020-09-14 20:22:56 +0200

    🦢 added likelihood ratio test

--- homework/adv-macroeconometrics/week-one/utils/var.py
+++ homework/adv-macroeconometrics/week-one/utils/var.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from statsmodels.tsa.vector_ar import var_model, svar_model
+from scipy.stats.distributions import chi2
 
 def var(df, lags = -1, **kwargs):
 
@@ -24,7 +25,37 @@ def svar(df, A = None, B = None, **kwargs):
         A=A, B=B, svar_type="AB"
     )
 
-    print("Finding optimum lag order...")
-    results = model.fit(**kwargs) 
+    results = model.fit(maxlags=4, **kwargs) 
 
     return results
+
+def make_lag_iter(maxlags):
+    return list(zip(
+            range(1, maxlags + 1),
+            range(0, maxlags)
+        ))[::-1]
+
+def lr_test(l1, l2, df_resid):
+    lr = df_resid*(l1 - l2)
+    p = chi2.sf(lr, 1)
+
+    return p < .05
+
+def select_lr(df, maxlags = 15, **kwargs):
+
+    lag_list = make_lag_iter(maxlags)
+
+    model = var_model.VAR(df, dates=df.index, freq="Q")
+
+    for more_l, less_l in lag_list:
+        big_model = model.fit(more_l, **kwargs)
+        small_model = model.fit(less_l, **kwargs)
+
+        reject = lr_test(big_model.llf, small_model.llf, big_model.df_resid)
+
+        if reject:
+            return more_l
+
+    else:
+        raise ValueError("Never rejected, increase maxlags")
+        
