cf1fe26fd614e56e72cd1ef691b877e24eaab48d
NoFishLikeIan <andreatitton96@gmail.com>
2020-09-19 19:22:58 +0200

    🐱‍🏍 fred data ingestion

---
+++ homework/adv-macroeconometrics/week-two/utils/ingest.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+
+def_url = "https://s3.amazonaws.com/files.fred.stlouisfed.org/fred-md/monthly/current.csv"
+
+def import_fred(url = def_url):
+    cur_dir = os.getcwd()
+    
+    data_dir = f"{cur_dir}/data"
+
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    file_path = f"{data_dir}/fred_data.csv"
+
+    if os.path.isfile(file_path):
+        print(f"Using cached data at data/fred_data.csv")
+
+        return pd.read_csv(file_path)
+    
+    else:
+        raw_df = pd.read_csv(url)
+        raw_df.to_csv(file_path, index=False)
+
+        return raw_df
