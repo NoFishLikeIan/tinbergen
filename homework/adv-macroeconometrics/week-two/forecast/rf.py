6f6ddf2fd50f7be3cc58ce77fa918bda2e9f12b8
NoFishLikeIan <andreatitton96@gmail.com>
2020-09-26 15:55:31 +0200

    🧹 better naming

--- homework/adv-macroeconometrics/week-two/forecast/rf.py
+++ homework/adv-macroeconometrics/week-two/forecast/rf.py
@@ -24,15 +24,17 @@ def construct_rf_chunks(df: pd.DataFrame, lags: int) -> Tuple[np.ndarray, np.nda
     delta_lags = relativedelta(months=lags)
 
     y = df.iloc[lags:]
-    N = y.shape[0]
 
-    X = np.empty((N, lags))
+    N = y.shape[0]
+    M = y.shape[1] if len(y.shape) > 1 else 1
+
+    X = np.empty((N, lags * M))
 
     for i, date in enumerate(y.index.tolist()):
         prev_date = date - delta_lags
         lagged_values = df.loc[prev_date:date - one_month]
 
-        X[i] = lagged_values.to_numpy()
+        X[i] = lagged_values.to_numpy().reshape(1, -1)[0]
 
     return X, y
 
@@ -44,28 +46,32 @@ def p_quad_loss(y_hat: np.ndarray, y:np.ndarray, alpha: float = 0.3) -> np.float
     return np.sum(np.where(e < 0, -(1-alpha)*sqr, -alpha*sqr))
 
 
+
 # TODO: make lag grid searchable
-def rf_forecast(df: pd.Series, lags: int = 12, **cv_kwargs):
+def make_forecaster(df: pd.DataFrame, lags: int = 12, search_params = {}, **cv_kwargs):
 
     X, y = construct_rf_chunks(df, lags)
 
+    search_params = {**grid_search_over, **search_params}
+
     rf = RandomForestRegressor()
 
-    clf = GridSearchCV(rf, grid_search_over, scoring=p_quad_loss, **cv_kwargs)
+    clf = GridSearchCV(rf, search_params, scoring=p_quad_loss, **cv_kwargs)
     clf.fit(X, y)
 
     model = clf.best_estimator_   
 
     def forecaster(X: np.ndarray) -> np.ndarray: 
-        X = X.reshape(1, lags)
+        X = X.reshape(1, -1)
 
-        y_hat = np.zeros(3)
+        y_hat = {}
 
-        all_prediction = [pred.predict(X)[0] for pred in model.estimators_]
         
-        y_hat[0] = np.percentile(all_prediction, 2.5)
-        y_hat[1] = model.predict(X)[0]
-        y_hat[2] = np.percentile(all_prediction, 97.5)
+        all_prediction = np.array([pred.predict(X)[0] for pred in model.estimators_])
+        
+        y_hat["lower_bound"] = np.percentile(all_prediction, 2.5, axis = 0)
+        y_hat["mean"] = np.mean(all_prediction, axis = 0)
+        y_hat["upper_bound"] = np.percentile(all_prediction, 97.5, axis = 0)
         
 
         return y_hat
