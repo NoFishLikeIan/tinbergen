8e13bd7b6aee3661e394f8bf81e2b7ce9a70b065
NoFishLikeIan <andreatitton96@gmail.com>
2020-10-13 19:25:30 +0200

    🐛 not-working NW estimator

--- homework/adv-macroeconometrics/week-three/cov_est.py
+++ homework/adv-macroeconometrics/week-three/cov_est.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-def white_var(X: np.ndarray, e: np.ndarray) -> np.ndarray:
+from typing import Tuple, NewType
+
+Dim = NewType("Dim", Tuple[int, int])
+
+def white_var(X: np.ndarray, e: np.ndarray, dimensions: Dim) -> np.ndarray:
     inverse = np.linalg.inv(X.T@X)
     
     # construct diagonal array
@@ -9,3 +13,34 @@ def white_var(X: np.ndarray, e: np.ndarray) -> np.ndarray:
     var_matrix = X.T@diag@X
     
     return inverse@var_matrix@inverse
+
+
+def nw_corrected(X: np.ndarray, e: np.ndarray, dimensions: Dim) -> np.ndarray:
+    inverse = np.linalg.inv(X.T@X)
+
+    N, T = dimensions
+    residuals = e.reshape(T, N)
+    res_X = X.reshape(T, N, -1)
+
+    contemp = (X.T@e)@(X.T@e).T
+
+    lagged = 0
+    M = T - 1
+
+    # TODO: Broadcast in linear algebra
+    for past_lag in range(M): 
+        weight = 1 - past_lag/T
+
+        gamma_l = 0
+
+        for i in range(N):
+            for t in range(past_lag+1, T):
+                sqrd_res = residuals[t, i]*residuals[t - past_lag, i]
+                coproduct = res_X[t, i].T@res_X[t - past_lag, i]
+
+                gamma_l += sqrd_res*coproduct
+
+        lagged += weight* (gamma_l + gamma_l.T)
+
+    breakpoint()
+    return
