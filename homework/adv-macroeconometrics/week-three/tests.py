41db7ec065e904775c12c0fba3232b41aa172ed7
NoFishLikeIan <andreatitton96@gmail.com>
2020-10-15 15:17:46 +0200

    🐛 pw test bug

--- homework/adv-macroeconometrics/week-three/tests.py
+++ homework/adv-macroeconometrics/week-three/tests.py
@@ -3,12 +3,11 @@ import numpy as np
 def panel_dw(resid: np.ndarray, N: int, T: int) -> np.float:
     
     variance = np.sum(np.square(resid))
-    
-    reshaped = resid.reshape(T, N)
-    
+        
     n_sum = np.empty((T-1, N))
     
     for t in range(1, T):
-        reshaped[t-1] = np.square(reshaped[t] - reshaped[t-1])
+        n_sum[t-1] = np.square(resid[t] - resid[t-1])
         
+
     return np.sum(n_sum) / variance
