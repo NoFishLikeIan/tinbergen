825a9114992502d77ac7272d07539d7b1a9eebc5
NoFishLikeIan <andreatitton96@gmail.com>
2020-10-18 12:28:57 +0200

    🏭 abstracted logic for variable formation

--- homework/adv-macroeconometrics/week-three/gmm.py
+++ homework/adv-macroeconometrics/week-three/gmm.py
@@ -10,34 +10,28 @@ from tests import panel_dw
 
 # --- Typings
 
-from typing import List, NewType, Union
+from typing import List, NewType, Union, Tuple
 
 # TODO: Instruments can be passed as a dataframe or as a number of lags of the regressors
 Instruments = NewType("Instruments", Union[pd.DataFrame, int])
 
+GmmVariables = NewType(
+    "GmmVariables", Tuple[np.ndarray, np.ndarray, np.ndarray, int, int]
+)
 
-def lagged_iv(
-    data: pd.DataFrame, dependent: str,
-    lagged_iv=1,
-    regressors: List[str] = [],
-    het_robust=False,
-    verbose=1
-):
 
-    if len(regressors) == 0:
-        regressors = data.index.get_level_values(0).tolist()
-
-    if lagged_iv < 1:
-        raise ValueError("Specify a numer")
-
-    data, iv_variables = transform.make_multi_lagged(
-        data, regressors, lags=lagged_iv)
+def extract_regs(data: pd.DataFrame, dependent: str, regressors: List[str], lag_instruments: int) -> GmmVariables:
+    """
+    Constructs the de-meaned Z, W, Y matrices. Returns the three matrices and the original size N, T
+    """
+    data, instruments = transform.make_multi_lagged(
+        data, regressors, lags=lag_instruments)
 
     # add the lagged dependent to the regression
     data, lagged_names = transform.make_multi_lagged(data, dependent, lags=1)
     regressors += lagged_names
 
-    Z_unmean, _, L = matrix.stack_to_columns(data, iv_variables)
+    Z_unmean, _, _ = matrix.stack_to_columns(data, instruments)
 
     Y_unmean, _, _ = matrix.stack_to_columns(data, dependent)
     W_unmean, N, T = matrix.stack_to_columns(data, regressors)
@@ -47,6 +41,30 @@ def lagged_iv(
     W = Q@W_unmean
     Y = Q@Y_unmean
     Z = Q@Z_unmean
+
+    return Z, W, Y, N, T
+
+
+def lagged_gmm(
+    data: pd.DataFrame, dependent: str,
+    lag_inst=1,
+    regressors: List[str] = [],
+    het_robust=False, iv=False,
+    verbose=1
+):
+
+    if len(regressors) == 0:
+        regressors = data.index.get_level_values(0).tolist()
+
+    if lag_inst < 1:
+        raise ValueError("Specify a number for lag_inst")
+
+    Z, W, Y, N, T = extract_regs(data, dependent, regressors, lag_inst)
+
+    if iv:
+        Pi = Z.T@Z
+    else:
+        Pi = np.identity
 
     W_f = Z@inv(Z.T@Z)@Z.T@W
 
@@ -74,7 +92,7 @@ if __name__ == '__main__':
 
     regs = ["d(lnY)", "INF"]
 
-    lagged_iv(
+    lagged_gmm(
         data, "S/Y",
-        regressors=regs, lagged_iv=1
+        regressors=regs, lag_inst=1
     )
