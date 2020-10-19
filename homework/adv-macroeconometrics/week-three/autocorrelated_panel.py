47f68877e4adbdb18fd3ef5663cb4eaf887c0694
NoFishLikeIan <andreatitton96@gmail.com>
2020-10-19 11:50:59 +0200

    🪐working towards Pesaran (2004)

---
+++ homework/adv-macroeconometrics/week-three/autocorrelated_panel.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import os
+
+from utils.time_series import autocorrelation
+
+if __name__ == '__main__':
+
+    residual_path = "data/resid.npy"
+
+    if not os.path.isfile(residual_path):
+        raise ValueError(f"File {residual_path} not found!")
+
+    resid = np.load(residual_path)
+
+    autocorrelation(resid)
