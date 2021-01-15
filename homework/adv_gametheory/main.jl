c0cb99e246b77563726ab7138dab059719494b1d
NoFishLikeIan <andreatitton96@gmail.com>
2021-01-15 18:13:36 +0100

    🧹 week two main

--- homework/adv_gametheory/main.jl
+++ homework/adv_gametheory/main.jl
@@ -1,11 +1,14 @@
 include("games.jl")
 include("shapley.jl")
 include("utils.jl")
+include("graphgames.jl")
 
-week = 1
+week = 2
 
 if week == 1
     include("one/one.jl")
     include("one/three.jl")
-end
+else week == 2
+    include("two/supply.jl")
 
+end
