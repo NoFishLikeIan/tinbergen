9408c2feb5d5fbc9f2e626157d958e1079cc0883
NoFishLikeIan <andreatitton96@gmail.com>
2021-01-22 12:14:39 +0100

    🌉 definitionof river game as link

--- homework/adv_gametheory/rivergame.jl
+++ homework/adv_gametheory/rivergame.jl
@@ -17,3 +17,27 @@ function findoptx(c::Vector{Float64}, e::Vector{Float64})
 
     return value.(x)
 end
+
+function makeriver(c::Vector{Float64}, e::Vector{Float64})
+    x̄ = findoptx(c, e)
+
+    N = collect(1:length(c))
+    L = [(i, i + 1) for i in 1:N[end - 1]]
+
+    v(r::UnitRange{Int64}) = v(collect(r))
+    v(n::Array{Any,1}) = isempty(n) ? 0 : v(S)
+    v(n::Int) = v([n])
+
+    function v(S::Array{Int})
+        # Normalized via c'x - c'e?
+        S = sort(S)
+        if S == N return c'x̄ end
+        if isempty(S) || !isconnected(S) return 0. end
+
+        x̄ₛ = findoptx(c[S], e[S])
+
+        return c[S]'x̄ₛ
+    end
+
+    return Graph(N, v, L)
+end
