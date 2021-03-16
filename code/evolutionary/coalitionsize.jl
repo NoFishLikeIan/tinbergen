10e9a05b1abf991f012b00efb14bf234d17d373c
NoFishLikeIan <andreatitton96@gmail.com>
2021-03-16 11:44:56 +0100

    🦉 final applications

--- code/evolutionary/coalitionsize.jl
+++ code/evolutionary/coalitionsize.jl
@@ -1,29 +1,62 @@
+using Plots
+using Printf
 
-M = 40
-N = 5
-ρs = [0.01, 0.1, 0.5]
-iter = 30
-Ts = 100
+function makerecurisve(M, N, ρ)
+    function EN(t)
+        if t == 0 return N end
 
-sim = zeros((length(ρs), Ts, iter))
-
-for (j, ρ) in enumerate(ρs)
-    for t in 1:Ts
-        print("$t / $Ts\r")
-        for i in 1:iter
-            
-            _, coalitions = evolve(M, N, T=t; swapgroup=ρ)
-
-            if isempty(coalitions)
-                coalsize = N
-            else
-                coalsize = mean(map(coal -> length(coal) * N, coalitions[end][2]))
-            end
-
-            sim[j, t, i] = coalsize
-        end
+        C = N * M / (M - t)
+        
+        return (1 - ρ) * EN(t - 1) + ρ * C
     end
-    print("\n")
+
+    return EN
 end
 
-average = reshape(mean(sim, dims=3), (length(ρs), Ts))
+M, N = 10, 5
+
+ρs = [0.0, 0.1, 0.2, 0.5, 1.0]
+aρ = 0.1
+
+ts = M - 1
+
+plot(
+    title="Expected group size for a given ρ | M=$M, N=$N", 
+    xaxis="t", yaxis="E[Nₜ]", legend=:topleft, dpi=200)
+
+for ρ in ρs
+    EN = makerecurisve(M, N, ρ)
+    Nₜ = EN.(1:ts)
+
+    plot!(1:ts, Nₜ, label="ρ=$(@sprintf("%.2f", ρ))", dpi=200)
+end
+
+savefig("plots/expected.png")
+
+
+function makeanalytical(M, N, ρ)
+    function EN(t)
+        if t == 1 return N end
+
+        first = N * (1 - ρ)^t
+
+        Σ = sum(
+            (1 - ρ)^i / (M - t + i)
+            for i in 0:(t - 1)
+        )
+
+        second = ρ * N * M * Σ
+
+        return first + second
+    end
+
+    return EN
+end
+
+
+ρ = .5
+EN_an = makeanalytical(M, N, ρ)
+EN_re = makerecurisve(M, N, ρ)
+
+EN_re.(1:ts)
+EN_an.(1:ts)
